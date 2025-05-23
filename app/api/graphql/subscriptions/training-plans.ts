eea24d582cb66ad492cb0dc6421245bcd4595de1
Oli Young <oliyoung@gmail.com>
2025-05-23 22:43:14 +0930

    Enhance training plan subscription with detailed event logging
    - Updated the `trainingPlanGenerated` subscription to log received events, improving traceability of athlete-specific training plan generation.
    - Changed the event subscription to utilize the correct PubSub event for training plan generation, ensuring accurate event handling.

--- app/api/graphql/subscriptions/training-plans.ts
+++ app/api/graphql/subscriptions/training-plans.ts
@@ -7,7 +7,10 @@ export const trainingPlanSubscriptions = {
     trainingPlanGenerated: {
         subscribe: (_: unknown, { athleteId }: { athleteId: string }, context: GraphQLContext,) => {
             logger.info({ athleteId }, 'Subscribing to trainingPlanGenerated for athlete');
-            return context.pubsub.subscribe(PubSubEvents.SessionLogGenerationSuccess, () => { });
+            return context.pubsub.subscribe(PubSubEvents.TrainingPlanGenerated, (payload: { trainingPlanGenerated: TrainingPlan }) => {
+                logger.debug({ athleteId, payloadAthleteId: payload.trainingPlanGenerated.athleteId }, 'Received trainingPlanGenerated event');
+                return payload.trainingPlanGenerated.athleteId === athleteId ? payload : null;
+            });
         },
         resolve: (payload: { trainingPlanGenerated: TrainingPlan }) => {
             logger.info({ payload }, "Resolving trainingPlanGenerated subscription payload");
