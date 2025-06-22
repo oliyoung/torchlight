51053b476e41de0a531a3676c2506a30d628b1d3
Oli Young <oliyoung@gmail.com>
2025-06-22 15:04:25 +0930

    Add GraphQLContext import to types.ts for improved type definitions

--- lib/types.ts
+++ lib/types.ts
@@ -1,4 +1,5 @@
 import { GraphQLResolveInfo, GraphQLScalarType, GraphQLScalarTypeConfig } from 'graphql';
+import { GraphQLContext } from '@/app/api/graphql/route';
 export type Maybe<T> = T | null;
 export type InputMaybe<T> = Maybe<T>;
 export type Exact<T extends { [key: string]: unknown }> = { [K in keyof T]: T[K] };
